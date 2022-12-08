f1208515cd4d565d031ea10d8156abe2b22d96d7
Dolgov David <dolgov.david.1993@gmail.com>
2022-12-08 04:13:35 +0300

    Initial commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,9 @@
+public class CreditPaymentService {
+
+    public double calculate(double sum,short years, float bid) {
+        float monthBid=bid/(100*12);                      // расчёт месечной процентной ставки
+        int month=years*12;
+        double result=sum*(monthBid/(1-Math.pow(1+monthBid,-month)));
+        return result;
+    }
+}
